24793e2e01e55fe45c9f61adb2192f0c264dee4f
Svetlana Margetová <svetlanamargetova@Svetlana-MacBook-Pro.local>
2015-07-27 23:13:54 +0200

    test

--- tedxostrava/settings.py
+++ tedxostrava/settings.py
@@ -175,7 +175,7 @@ import os
 #BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 #STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 STATICFILES_DIRS = (
