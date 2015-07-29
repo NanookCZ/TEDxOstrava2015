39e907e199da57bcb40a9460f5af01a6d399b386
Svetlana Margetová <svetlanamargetova@Svetlana-MacBook-Pro.local>
2015-07-29 09:23:57 +0200

    url

--- tedxostrava/urls.py
+++ tedxostrava/urls.py
@@ -10,12 +10,15 @@ from partners.views import PartnerListAPIView
 from speakers.views import SpeakerListAPIView, SpeakerDetailAPIView
 from tedx.views import TEDxListAPIView, AboutAppListAPIView
 from news.views import NewsListAPIView, TypeListAPIView, NewsDetailAPIView
+from django.views.generic.base import RedirectView
+
 
 router = routers.DefaultRouter()
 
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls), name='admin'),
+    url(r'^$', RedirectView.as_view(url='admin/', permanent=False), name='index')
     url(r'^api/auth/token/$', 'rest_framework_jwt.views.obtain_jwt_token'),
     url(r'^api/auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/$', 'tedxostrava.views.tedx_api_home', name="api_home"),
