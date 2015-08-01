188245cc402d50d0e22fa351b071a192a9fbab38
Svetlana Margetová <svetlanamargetova@Svetlana-MacBook-Pro.local>
2015-08-01 07:56:45 +0200

    test

---
+++ partners/migrations/0002_auto_20150801_0555.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import cloudinary.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('partners', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='partner',
+            name='partner_logo',
+            field=cloudinary.models.CloudinaryField(max_length=255, null=True, verbose_name='image', blank=True),
+        ),
+    ]
--- tedxostrava/settings.py
+++ tedxostrava/settings.py
@@ -146,8 +146,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
-""" 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 from django.utils.translation import ugettext
