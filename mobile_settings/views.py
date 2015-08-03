0aeaf4a434a2a1d534fcec6bd83fccee12219f40
Svetlana Margetová <svetlanamargetova@Svetlana-MacBook-Pro.local>
2015-08-03 12:33:32 +0200

    test

--- mobile_settings/views.py
+++ mobile_settings/views.py
@@ -1,11 +1,12 @@
 from .serializers import MenuSerializer
-from .models import Menu
+from .models import Menu, Language
 from rest_framework import generics,  permissions
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 
 
 class MenuListAPIView(generics.ListAPIView):
-	queryset = Menu.objects.filter(active = True, language = 'CS')
+	language = Language.objects.get(code = 'CS')
+	queryset = Menu.objects.filter(active = True, language = language)
 	serializer_class = MenuSerializer
 	paginate_by = 10
