e3b0e370790aae72267c141ab4c16d51663e9786
Svetlana Margetová <svetlanamargetova@Svetlana-MacBook-Pro.local>
2015-08-03 09:10:26 +0200

    test

--- mobile_settings/apps.py
+++ mobile_settings/apps.py
@@ -1,5 +1,6 @@
 from django.apps import AppConfig
-
+from django.utils.translation import ugettext_lazy as _
+ 
 class YourAppConfig(AppConfig):
     name = 'mobile_settings'
-    verbose_name = 'Mobile settings'
+    verbose_name = _('Mobile settings')
