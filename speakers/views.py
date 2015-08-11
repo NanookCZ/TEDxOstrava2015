eb72b970fdbc21d3710335cd73014c4e06cc7534
Svetlana Margetová <svetlanamargetova@Svetlana-MacBook-Pro.local>
2015-08-11 13:55:00 +0200

    a

--- speakers/views.py
+++ speakers/views.py
@@ -7,6 +7,7 @@ from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from mobile_settings.models import Language
 
+
 class SpeakerListAPIView(generics.ListAPIView):
 	language = Language.objects.get(code = 'CS')
 	queryset = Speaker.objects.filter(active = True, published = True, language = language)
