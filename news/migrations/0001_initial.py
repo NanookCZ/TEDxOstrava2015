98208d96ea1b9754c28eb73758eb4b03d9a99460
Svetlana <svetlana@margetova.eu>
2015-09-04 21:32:27 +0200

    migrations

---
+++ news/migrations/0001_initial.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+import cloudinary.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('speakers', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('mobile_settings', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='News',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=50, verbose_name='title')),
+                ('description', models.TextField(verbose_name='description')),
+                ('link', models.URLField(null=True, blank=True)),
+                ('created_date', models.DateTimeField(auto_now_add=True, verbose_name='created_date')),
+                ('updated_date', models.DateTimeField(auto_now=True, verbose_name='updated_date')),
+                ('publication_date', models.DateField(null=True, verbose_name='publication_date', blank=True)),
+                ('active', models.BooleanField(default=True, verbose_name='active')),
+                ('is_published', models.BooleanField(default=False, verbose_name='is_published')),
+                ('main_news', models.BooleanField(default=False, verbose_name='main_news')),
+                ('author', models.ForeignKey(verbose_name='User', to=settings.AUTH_USER_MODEL)),
+                ('language', models.ForeignKey(verbose_name='Language', blank=True, to='mobile_settings.Language', null=True)),
+                ('news_image', models.ForeignKey(blank=True, to='mobile_settings.Image', null=True)),
+            ],
+            options={
+                'verbose_name': 'News',
+                'verbose_name_plural': 'News',
+            },
+        ),
+        migrations.CreateModel(
+            name='Type',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=50, verbose_name='title')),
+                ('icon', cloudinary.models.CloudinaryField(max_length=255, null=True, verbose_name='image', blank=True)),
+                ('language', models.ForeignKey(verbose_name='Language', blank=True, to='mobile_settings.Language', null=True)),
+            ],
+            options={
+                'verbose_name': 'Type',
+                'verbose_name_plural': 'Types',
+            },
+        ),
+        migrations.AddField(
+            model_name='news',
+            name='news_type',
+            field=models.ForeignKey(verbose_name='Type', to='news.Type'),
+        ),
+        migrations.AddField(
+            model_name='news',
+            name='speaker',
+            field=models.ForeignKey(verbose_name='Speaker', blank=True, to='speakers.Speaker', null=True),
+        ),
+    ]
---
+++ tedx/migrations/0001_initial.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import cloudinary.models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mobile_settings', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='AboutApp',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=50, verbose_name='title')),
+                ('description', models.TextField(verbose_name='description')),
+                ('website', models.URLField(null=True, verbose_name='website', blank=True)),
+                ('created_date', models.DateTimeField(auto_now_add=True, verbose_name='created_date')),
+                ('updated_date', models.DateTimeField(auto_now=True, verbose_name='updated_date')),
+                ('active', models.BooleanField(default=True, verbose_name='active')),
+                ('logo', cloudinary.models.CloudinaryField(max_length=255, null=True, verbose_name='image', blank=True)),
+                ('language', models.ForeignKey(verbose_name='Language', blank=True, to='mobile_settings.Language', null=True)),
+            ],
+            options={
+                'verbose_name': 'About app',
+                'verbose_name_plural': 'About app',
+            },
+        ),
+        migrations.CreateModel(
+            name='TEDx',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=50, verbose_name='title')),
+                ('website', models.URLField(null=True, verbose_name='website', blank=True)),
+                ('created_date', models.DateTimeField(auto_now_add=True, verbose_name='created_date')),
+                ('updated_date', models.DateTimeField(auto_now=True, verbose_name='updated_date')),
+                ('active', models.BooleanField(default=True, verbose_name='active')),
+                ('is_published', models.BooleanField(default=False)),
+                ('tedx_logo', cloudinary.models.CloudinaryField(max_length=255, null=True, verbose_name='image', blank=True)),
+                ('event_start', models.DateField(null=True, verbose_name='event_start', blank=True)),
+            ],
+            options={
+                'verbose_name': 'TEDx',
+                'verbose_name_plural': 'TEDx',
+            },
+        ),
+    ]
