939c6269bbfe871d8135a6604e0be8dcefaac61f
Svetlana <svetlana@margetova.eu>
2015-09-05 10:48:06 +0200

    a

---
+++ events/migrations/0002_theme_banner.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mobile_settings', '0001_initial'),
+        ('events', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='theme',
+            name='banner',
+            field=models.ForeignKey(blank=True, to='mobile_settings.Image', null=True),
+        ),
+    ]
---
+++ news/migrations/0002_news_news_image.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mobile_settings', '0001_initial'),
+        ('news', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='news',
+            name='news_image',
+            field=models.ForeignKey(blank=True, to='mobile_settings.Image', null=True),
+        ),
+    ]
