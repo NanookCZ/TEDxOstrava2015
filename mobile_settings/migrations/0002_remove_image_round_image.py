19177ae5ecb6b4f8b7e16c3c0d4dbe9d23b736a3
Svetlana <svetlana@margetova.eu>
2015-09-05 11:53:05 +0200

    a

---
+++ mobile_settings/migrations/0002_remove_image_round_image.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mobile_settings', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='image',
+            name='round_image',
+        ),
+    ]
---
+++ mobile_settings/migrations/0003_image_round_image.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('mobile_settings', '0002_remove_image_round_image'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='image',
+            name='round_image',
+            field=models.URLField(null=True, blank=True),
+        ),
+    ]
