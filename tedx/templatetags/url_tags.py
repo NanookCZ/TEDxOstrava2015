76209eef87fd4fb7a1e3f0b51ac0446ac8050750
Svetlana <svetlana@margetova.eu>
2015-09-11 09:32:56 +0200

    url

--- tedx/templatetags/url_tags.py
+++ tedx/templatetags/url_tags.py
@@ -1,8 +1,9 @@
 from django import template
 from django.core.urlresolvers import request 
+from django.contrib.sites.models import RequestSite    
 
 register = template.Library()
 
 @register.simple_tag
 def get_url():
-    return request.get_absolute_url()
+    return RequestSite(request).domain
