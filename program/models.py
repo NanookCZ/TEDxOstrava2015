8cf4630d6217f22795c170fb3ec7e7695e97c3b5
Svetlana <svetlana@margetova.eu>
2015-10-11 22:12:27 +0200

    a

--- program/models.py
+++ program/models.py
@@ -42,10 +42,10 @@ class Slot(models.Model):
 	def __unicode__(self):
 		return "%s" %(self.kind)
 
-def validate_date(date):
-	presentation = Presentation.objects.get(id = self.id)
-	if date != presentation.slot.start:
-		raise ValidationError("Error")
+	def validate_date(date):
+		slot = Slot.objects.get(id = self.id)
+		if date != slot.start:
+			raise ValidationError("Error")
 
 class Presentation(models.Model):
 	language = models.ForeignKey(Language, blank = True, null = True, default = 1)
