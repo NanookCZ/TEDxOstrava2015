8e4c5e127b77edbefeb933a4a978b9b2f15fa084
Svetlana <svetlana@margetova.eu>
2015-10-24 14:36:34 +0200

    a

--- mobile_settings/admin.py
+++ mobile_settings/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from .models import Menu, Language, Image
+from django.utils.translation import ugettext_lazy as _
 
 admin.site.register(Language)
 # Register your models here.
 admin.site.register(Menu)
 
 class ImageAdmin(admin.ModelAdmin):
-	fields = ('image_title', 'image', 'active', 'image_thumb')
+	fields = (_('image_title'), 'image', 'active', _('image_thumb'))
 	readonly_fields = ['image_thumb']
 	list_display = ('image_title', 'active', 'image_thumb')
 	
